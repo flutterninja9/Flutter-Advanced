866443d7cbdf7ad74705da100849e047243c694e
Anirudh Pratap Singh <anirudhisgreat1200@gmail.com>
2021-01-20 12:59:51 +0530

    added tests for data_connection_checker

--- clean_architecture_dog_app/lib/features/dog_app/data/repository/image_object_repository_impl.dart
+++ clean_architecture_dog_app/lib/features/dog_app/data/repository/image_object_repository_impl.dart
@@ -1,6 +1,6 @@
 import 'package:clean_architecture_dog_app/core/errors/error_model.dart';
 import 'package:clean_architecture_dog_app/core/errors/exceptions.dart';
-import 'package:clean_architecture_dog_app/core/platform/connection_status.dart';
+import 'package:clean_architecture_dog_app/core/network/connection_status.dart';
 import 'package:clean_architecture_dog_app/features/dog_app/data/data_sources/image_object_remote_data_source.dart';
 import 'package:clean_architecture_dog_app/features/dog_app/domain/enitities/image_object.dart';
 import 'package:clean_architecture_dog_app/features/dog_app/domain/repository/image_object_repository.dart';
---
+++ clean_architecture_dog_app/test/core/network/network_info_test.dart
@@ -0,0 +1,38 @@
+import 'package:clean_architecture_dog_app/core/network/connection_status.dart';
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class MockDataConnectionChecker extends Mock implements DataConnectionChecker {}
+
+void main() {
+  NetworkInfoImpl networkInfoImpl;
+  MockDataConnectionChecker dataConnectionChecker;
+
+  setUp(() {
+    dataConnectionChecker = MockDataConnectionChecker();
+    networkInfoImpl = NetworkInfoImpl(dataConnectionChecker);
+  });
+
+  group('getNetworkConnectionStatus', () {
+    test('should give the network connection status when connected', () async {
+      //arrange
+      when(dataConnectionChecker.hasConnection).thenAnswer((_) async => true);
+      //act
+      final result = await networkInfoImpl.isConnected;
+      //assert
+      verify(dataConnectionChecker.hasConnection);
+      expect(result, true);
+    });
+    test('should give the network connection status when not_connected',
+        () async {
+      //arrange
+      when(dataConnectionChecker.hasConnection).thenAnswer((_) async => false);
+      //act
+      final result = await networkInfoImpl.isConnected;
+      //assert
+      verify(dataConnectionChecker.hasConnection);
+      expect(result, false);
+    });
+  });
+}
