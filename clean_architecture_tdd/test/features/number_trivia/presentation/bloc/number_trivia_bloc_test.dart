4066a275bce499b18944bf6c871a7f83f61a07d4
Anirudh Pratap Singh <anirudhisgreat1200@gmail.com>
2021-01-23 11:02:17 +0530

    refactored

---
+++ clean_architecture_tdd/test/features/number_trivia/presentation/bloc/number_trivia_bloc_test.dart
@@ -0,0 +1,24 @@
+import 'package:clean_architecture_tdd/core/utils/input_converter.dart';
+import 'package:clean_architecture_tdd/features/number_trivia/domain/use_cases/get_concrete_number_trivia.dart';
+import 'package:clean_architecture_tdd/features/number_trivia/domain/use_cases/get_random_number_trivia.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class MockGetConcreteNumberTrivia extends Mock
+    implements GetConcreteNumberTrivia {}
+
+class MockGetRandomNumberTrivia extends Mock implements GetRandomNumberTrivia {}
+
+class MockInputConverter extends Mock implements InputConverter {}
+
+void main() {
+  MockGetConcreteNumberTrivia getConcreteNumberTrivia;
+  MockGetRandomNumberTrivia getRandomNumberTrivia;
+  MockInputConverter inputConverter;
+
+  setUp(() {
+    getConcreteNumberTrivia = MockGetConcreteNumberTrivia();
+    getRandomNumberTrivia = MockGetRandomNumberTrivia();
+    inputConverter = MockInputConverter();
+  });
+}
