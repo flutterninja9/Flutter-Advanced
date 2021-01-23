205e3532ffa11797ae0c7d173520c4050535701d
Anirudh Pratap Singh <anirudhisgreat1200@gmail.com>
2021-01-23 10:56:14 +0530

    initiated bloc test

--- clean_architecture_tdd/lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ clean_architecture_tdd/lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -1,14 +1,27 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
-import 'package:clean_architecture_tdd/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
+
+import 'package:clean_architecture_tdd/core/utils/input_converter.dart';
+import 'package:clean_architecture_tdd/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:clean_architecture_tdd/features/number_trivia/domain/use_cases/get_concrete_number_trivia.dart';
+import 'package:clean_architecture_tdd/features/number_trivia/domain/use_cases/get_random_number_trivia.dart';
 
 part 'number_trivia_event.dart';
 part 'number_trivia_state.dart';
 
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
+  final GetConcreteNumberTrivia getConcreteNumberTrivia;
+  final GetRandomNumberTrivia getRandomNumberTrivia;
+  final InputConverter inputConverter;
+
+  NumberTriviaBloc({
+    @required this.getConcreteNumberTrivia,
+    @required this.getRandomNumberTrivia,
+    @required this.inputConverter,
+  });
   @override
   NumberTriviaState get initialState => Empty();
 
