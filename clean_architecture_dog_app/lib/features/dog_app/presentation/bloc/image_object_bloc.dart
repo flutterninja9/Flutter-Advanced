714c221db4ad35d4d7e32226377d63f22f48d0fe
Anirudh Pratap Singh <anirudhisgreat1200@gmail.com>
2021-01-24 13:41:09 +0530

    initiated dog_app bloc_logic_2 implementation

--- clean_architecture_dog_app/lib/features/dog_app/presentation/bloc/image_object_bloc.dart
+++ clean_architecture_dog_app/lib/features/dog_app/presentation/bloc/image_object_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:clean_architecture_dog_app/core/usecases/usecase.dart';
 import 'package:clean_architecture_dog_app/features/dog_app/domain/enitities/image_object.dart';
 import 'package:clean_architecture_dog_app/features/dog_app/domain/usecases/get_image_object.dart';
 import 'package:equatable/equatable.dart';
@@ -25,5 +26,14 @@ class ImageObjectBloc extends Bloc<ImageObjectEvent, ImageObjectState> {
   @override
   Stream<ImageObjectState> mapEventToState(
     ImageObjectEvent event,
-  ) async* {}
+  ) async* {
+    if (event is GetObjectForImage) {
+      yield Loading();
+      final imageModelOrError = await imageObject(NoParams());
+      yield imageModelOrError.fold(
+        (error) => Failed(SERVER_FAILURE),
+        (imageData) => Loaded(imageData),
+      );
+    }
+  }
 }
--- clean_architecture_dog_app/test/features/dog_app/presentation/bloc/image_object_bloc_test.dart
+++ clean_architecture_dog_app/test/features/dog_app/presentation/bloc/image_object_bloc_test.dart
@@ -1,5 +1,9 @@
+import 'package:clean_architecture_dog_app/core/errors/error_model.dart';
+import 'package:clean_architecture_dog_app/core/usecases/usecase.dart';
+import 'package:clean_architecture_dog_app/features/dog_app/domain/enitities/image_object.dart';
 import 'package:clean_architecture_dog_app/features/dog_app/domain/usecases/get_image_object.dart';
 import 'package:clean_architecture_dog_app/features/dog_app/presentation/bloc/image_object_bloc.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
@@ -18,5 +22,51 @@ void main() {
     expect(bloc.initialState, equals(Empty()));
   });
 
-  group('GetObjectImage', () {});
+  group('GetObjectImage', () {
+    final tImageObject =
+        ImageObject(imageURL: 'https://dog.ceo/api/breeds/image/random');
+    test('should get the data from the getImageObject use-case', () async {
+      //arrange
+      when(getImageObject(NoParams()))
+          .thenAnswer((_) async => Right(tImageObject));
+      //act
+      bloc.dispatch(GetObjectForImage());
+      await untilCalled(getImageObject(NoParams()));
+      //assert
+      verify(getImageObject(NoParams()));
+    });
+    test('should emit [Loading,Loaded] while calling the data resources',
+        () async {
+      //arrange
+      when(getImageObject(NoParams()))
+          .thenAnswer((_) async => Right(tImageObject));
+      //assert
+      expectLater(
+          Empty(),
+          emitsInOrder([
+            Empty(),
+            Loading(),
+            Loaded(tImageObject),
+          ]));
+      //act
+      bloc.dispatch(GetObjectForImage());
+    });
+    test(
+        'should emit [Loading,ErrorModel] while calling the data resources is failed',
+        () async {
+      //arrange
+      when(getImageObject(NoParams()))
+          .thenAnswer((_) async => Left(ServerFailure()));
+      //assert
+      expectLater(
+          Empty(),
+          emitsInOrder([
+            Empty(),
+            Loading(),
+            Failed(SERVER_FAILURE),
+          ]));
+      //act
+      bloc.dispatch(GetObjectForImage());
+    });
+  });
 }
