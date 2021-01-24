d6d83a70d716a9abf6a5a210f073c769465f34f2
Anirudh Pratap Singh <anirudhisgreat1200@gmail.com>
2021-01-24 20:58:41 +0530

    initiated dependency_injection

--- clean_architecture_tdd/lib/injection_container.dart
+++ clean_architecture_tdd/lib/injection_container.dart
@@ -1,3 +1,9 @@
+import 'package:clean_architecture_tdd/core/utils/input_converter.dart';
+import 'package:clean_architecture_tdd/features/number_trivia/data/repository/number_trivia_repository_impl.dart';
+import 'package:clean_architecture_tdd/features/number_trivia/data/sources/number_trivia_remote_data_source.dart';
+import 'package:clean_architecture_tdd/features/number_trivia/domain/repository/number_trivia_repository.dart';
+import 'package:clean_architecture_tdd/features/number_trivia/domain/use_cases/get_concrete_number_trivia.dart';
+import 'package:clean_architecture_tdd/features/number_trivia/domain/use_cases/get_random_number_trivia.dart';
 import 'package:clean_architecture_tdd/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
 import 'package:get_it/get_it.dart';
 
@@ -11,9 +17,20 @@ void init() {
         inputConverter: sl(),
         getRandomNumberTrivia: sl(),
       ));
-  //! Core
 
+  //* Registering use-cases or Singletons
+  sl.registerLazySingleton(() => GetConcreteNumberTrivia(sl()));
+  sl.registerLazySingleton(() => GetRandomNumberTrivia(sl()));
+
+  //* Lazy Loading repository
+  sl.registerLazySingleton<NumberTriviaRepository>(
+      () => NumberTriviaRepositoryImpl(
+            remoteDataSource: sl(),
+            localDataSource: sl(),
+            networkInfo: sl(),
+          ));
+
+  //! Core
+  sl.registerLazySingleton(() => InputConverter());
   //! External
 }
-
-// Registering use-cases or Singletons
