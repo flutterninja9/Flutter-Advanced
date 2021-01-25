c61800a159230607260fc50d9b184d20db1b0101
Anirudh Pratap Singh <anirudhisgreat1200@gmail.com>
2021-01-25 11:27:20 +0530

    ui completed

--- clean_architecture_tdd/lib/main.dart
+++ clean_architecture_tdd/lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:clean_architecture_tdd/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
 import 'package:clean_architecture_tdd/features/number_trivia/presentation/screens/number_trivia_screen.dart';
 import 'package:clean_architecture_tdd/injection_container.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await init(); //! Dependency Injection For all the various features in a single call
   runApp(MyApp());
 }
@@ -14,7 +17,11 @@ class MyApp extends StatelessWidget {
       title: 'Number Trivia App',
       theme:
           ThemeData(primarySwatch: Colors.pink, accentColor: Colors.pinkAccent),
-      home: NumberTriviaScreen(),
+      home: BlocProvider(
+        builder: (_) => sl<NumberTriviaBloc>(),
+        child: NumberTriviaScreen(),
+      ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
