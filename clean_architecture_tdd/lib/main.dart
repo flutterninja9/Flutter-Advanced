f8a37da7d2eb8924b9fd34b85f5a868fdc524a2a
Anirudh Pratap Singh <anirudhisgreat1200@gmail.com>
2021-01-25 11:44:17 +0530

    final, but buggy

--- clean_architecture_tdd/lib/main.dart
+++ clean_architecture_tdd/lib/main.dart
@@ -1,8 +1,9 @@
-import 'package:clean_architecture_tdd/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
 import 'package:clean_architecture_tdd/features/number_trivia/presentation/screens/number_trivia_screen.dart';
 import 'package:clean_architecture_tdd/injection_container.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,15 +14,15 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Number Trivia App',
-      theme:
-          ThemeData(primarySwatch: Colors.pink, accentColor: Colors.pinkAccent),
-      home: BlocProvider(
-        builder: (_) => sl<NumberTriviaBloc>(),
-        child: NumberTriviaScreen(),
+    return BlocProvider(
+      builder: (context) => sl<NumberTriviaBloc>(),
+      child: MaterialApp(
+        title: 'Number Trivia App',
+        theme: ThemeData(
+            primarySwatch: Colors.pink, accentColor: Colors.pinkAccent),
+        home: NumberTriviaScreen(),
+        debugShowCheckedModeBanner: false,
       ),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
