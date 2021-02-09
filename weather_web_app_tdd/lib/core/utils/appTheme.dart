4cd5268487c6ec6710d9f659ae43e162f73f224c
Anirudh Pratap Singh <anirudhisgreat1200@gmail.com>
2021-02-09 22:42:37 +0530

    added weather tdd

---
+++ weather_web_app_tdd/lib/core/utils/appTheme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData primaryAppTheme = ThemeData(
+    primaryColor: Color.fromRGBO(132, 220, 198, 1),
+    accentColor: Color.fromRGBO(165, 255, 214, 1),
+    buttonColor: Color.fromRGBO(255, 104, 107, 1),
+    cardColor: Color.fromRGBO(255, 166, 158, 1),
+    canvasColor: Colors.white,
+  );
+}
