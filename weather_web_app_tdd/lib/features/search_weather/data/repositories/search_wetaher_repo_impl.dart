f8b3995e64b5e7dc9e211a5f2c17990733069a16
Anirudh Pratap Singh <anirudhisgreat1200@gmail.com>
2021-02-10 19:23:21 +0530

    device online tests done

--- weather_web_app_tdd/lib/features/search_weather/data/repositories/search_wetaher_repo_impl.dart
+++ weather_web_app_tdd/lib/features/search_weather/data/repositories/search_wetaher_repo_impl.dart
@@ -1,4 +1,6 @@
+import 'package:weather_web_app_tdd/core/errors/exceptions.dart';
 import 'package:weather_web_app_tdd/core/platform/network_info.dart';
+import 'package:weather_web_app_tdd/features/search_weather/data/models/weather_model.dart';
 import 'package:weather_web_app_tdd/features/search_weather/data/sources/weather_local_data_source.dart';
 import 'package:weather_web_app_tdd/features/search_weather/data/sources/weather_remote_data_source.dart';
 
@@ -16,7 +18,15 @@ class WeatherRepositoryImpl implements WeatherRepository {
       this.networkInfo, this.localDataSource, this.remoteDataSource);
 
   @override
-  Future<Either<Failure, Weather>> searchWeather({String city}) {
-    return null;
+  Future<Either<Failure, Weather>> searchWeather({String city}) async {
+    networkInfo.isConnected;
+    try {
+      final WeatherModel weather =
+          await remoteDataSource.searchWeather(city: city);
+      localDataSource.cacheWeather(weather);
+      return Right(weather);
+    } on ServerException {
+      return Left(ServerFailure());
+    }
   }
 }
--- weather_web_app_tdd/test/features/weather_search/data/repository/weather_repository_impl_test.dart
+++ weather_web_app_tdd/test/features/weather_search/data/repository/weather_repository_impl_test.dart
@@ -1,6 +1,10 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:weather_web_app_tdd/core/errors/exceptions.dart';
+import 'package:weather_web_app_tdd/core/errors/failures.dart';
 import 'package:weather_web_app_tdd/core/platform/network_info.dart';
+import 'package:weather_web_app_tdd/features/search_weather/data/models/weather_model.dart';
 import 'package:weather_web_app_tdd/features/search_weather/data/repositories/search_wetaher_repo_impl.dart';
 import 'package:weather_web_app_tdd/features/search_weather/data/sources/weather_local_data_source.dart';
 import 'package:weather_web_app_tdd/features/search_weather/data/sources/weather_remote_data_source.dart';
@@ -29,5 +33,66 @@ void main() {
       remoteDataSource,
     );
   });
+
+// Tests for device online and offline
+
+  test('should check if the device is online', () async {
+    //arrange
+    when(networkInfo.isConnected).thenAnswer((_) async => true);
+    //act
+    weatherRepositoryImpl.searchWeather(city: 'London');
+    //assert
+    verify(networkInfo.isConnected);
+  });
+  group('device online', () {
+    final tWeather = WeatherModel(
+      pressure: 1010,
+      temprature: 272.74,
+      description: 'overcast clouds',
+      city: 'London',
+      main: 'Clouds',
+      humidity: 69,
+    );
+    setUp(() {
+      when(networkInfo.isConnected).thenAnswer((_) async => true);
+    });
+    // Tests for deive Online
+    test(
+        'should return a WeatherModel when the call to rmeoteData source is successfull',
+        () async {
+      //arrange
+      when(remoteDataSource.searchWeather(city: 'London'))
+          .thenAnswer((_) async => tWeather);
+      //act
+      final result = await weatherRepositoryImpl.searchWeather(city: 'London');
+      //assert
+      verify(remoteDataSource.searchWeather(city: 'London'));
+      expect(result, Right(tWeather));
+    });
+    test(
+        'should cache the data locally when the call to remote data is successfull',
+        () async {
+      //arrange
+      when(remoteDataSource.searchWeather(city: 'London'))
+          .thenAnswer((_) async => tWeather);
+      //act
+      final result = await weatherRepositoryImpl.searchWeather(city: 'London');
+      //assert
+      verify(remoteDataSource.searchWeather(city: 'London'));
+      verify(localDataSource.cacheWeather(tWeather));
+    });
+    test(
+        'should return ServerFailure when call to rmeote data source is un-successfull',
+        () async {
+      //arrange
+      when(remoteDataSource.searchWeather(city: 'London'))
+          .thenThrow(ServerException());
+      //act
+      final result = await weatherRepositoryImpl.searchWeather(city: 'London');
+      //assert
+      verify(remoteDataSource.searchWeather(city: 'London'));
+      verifyZeroInteractions(localDataSource);
+      expect(result, equals(Left(ServerFailure())));
+    });
+  });
 }
-//Todo Start writing the tests for deviceOnline & deviceOffline case
