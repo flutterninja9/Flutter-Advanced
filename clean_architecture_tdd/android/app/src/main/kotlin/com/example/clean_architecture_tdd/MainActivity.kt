32f50577f30d2919e8577621d0472b26e49545c4
Anirudh Singh <anirudhisgreat1200@gmail.com>
2021-06-13 09:20:42 +0530

    removed number_trivia app

--- clean_architecture_tdd/android/app/src/main/kotlin/com/example/clean_architecture_tdd/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.clean_architecture_tdd
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- clean_architecture_tdd/lib/core/error/exceptions.dart
+++
@@ -1,5 +0,0 @@
-// thrown when mis-things happen in network related states
-class ServerException implements Exception {}
-
-// Thrown when mis-things happen in caching
-class CacheException implements Exception {}
